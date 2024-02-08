974b40e9fa61eda401d4f3ce3fb38181b9dbb26e
Vitaly <vspetrovwork@gmail.com>
2024-02-08 17:52:07 +0300

    end at 19 bideo

--- index.ts
+++ index.ts
@@ -229,3 +229,44 @@ function sendInvoice(
 }
 
 console.log(sendInvoice(monthPayments, electricityUserData, waterUserData));
+
+// ______________________________
+// * Tuples
+
+const userDataTuple: [boolean, number, ...string[]] = [
+  true,
+  40,
+  "Jhon",
+  "Jhon",
+  "Jhon",
+  "Jhon",
+];
+
+const [isMan, age, ...userNameP] = userDataTuple;
+console.log(userNameP);
+
+// ______________________________
+// Union types
+
+const newMessage: string | number = "hello";
+const massages: string[] | number[] = [3213, 3232];
+const massages2: string[] | number[] = ["21wqe", "211"];
+// Если хочется кашу - используй кортежи
+
+// function printMsg(msg: string | number): void {
+//   console.log(msg);
+// }
+
+// ______________________________
+// Сужение типов
+
+function printMsg(msg: string | number): void {
+  if (typeof msg === "string") {
+    console.log(msg.toUpperCase());
+  } else {
+    console.log(msg.toExponential(3));
+  }
+}
+
+printMsg("qweqw");
+printMsg(12);
